866c445683b16467d7d52ca6cc860af63c28dc3b
Ozan Kasikci <kasikciozan@yandex.com>
2019-12-14 00:24:49 +0100

    makeRange exclusive min value

--- pkg/slices/slices.go
+++ pkg/slices/slices.go
@@ -36,8 +36,9 @@ func RemoveStringFromSlice(s []string, r string) []string {
 	return s
 }
 
+// min: inclusive, max: exclusive
 func MakeRange(min, max int) []int {
-	a := make([]int, max-min+1)
+	a := make([]int, max-min)
 	for i := range a {
 		a[i] = min + i
 	}
